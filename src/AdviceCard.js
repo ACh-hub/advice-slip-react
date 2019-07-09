6e0e99e6f538a22e350325c006f08b3836bcc699
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-07-09 22:57:31 +0200

    advice card in progress, i need placeholder pictures

---
+++ src/AdviceCard.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './AdviceCard.css';
+
+class AdviceCard extends Component{
+
+    render(){
+        const {advice} = this.props;
+        return (
+            <div className="AdviceCard">
+            <picture srcSet="" alt="Enlightened Halibut"></picture>
+            <h2>Enlightened Halibut Says</h2>
+            <p>{advice}</p>
+            <button>Gib Advice!</button>
+            </div>
+        );
+    }
+}
+
+export default AdviceCard;
