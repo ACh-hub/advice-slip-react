e397799e849eed1dd696c06b9d6e5e261b878f17
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-07-10 22:23:19 +0200

    added halibut of truth img

--- src/components/AdviceCard.js
+++ src/components/AdviceCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './AdviceCard.css';
+import halibut from '../components/halibut-of-truth-small.png';
 
 const AdviceCard = ({advice, clickButtonRandom}) => {
         return (
             <div className="AdviceCard">
-            <picture srcSet="" alt="Enlightened Halibut"></picture>
+            <img src={halibut} style={{width: 100+`vw`}} alt="Halibut of truth"></img>
             <h2>Enlightened Halibut Says</h2>
             <p>{advice}</p>
             <button onClick={clickButtonRandom}>Gib Advice!</button>
